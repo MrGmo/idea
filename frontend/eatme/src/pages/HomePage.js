370c09d5a0a3d724750a1ecab0f6ff59c306775c
Guillermo <gaguilar128@gmail.com>
2022-01-04 23:46:04 -0600

    Working on search bar

--- frontend/eatme/src/pages/HomePage.js
+++ frontend/eatme/src/pages/HomePage.js
@@ -4,22 +4,36 @@ import FoodItemList from "../components/FoodItemList.js";
 function HomePage() {
     const [userInput, setUserInput] = useState('')
     const [lat, setLat] = useState(null)
-    const [long, setLong] = useState(null)
+    const [lng, setLng] = useState(null)
     const [miles, setMiles] = useState(0)
+    const [zipcode, setZipcode] = useState(null)
     const [foodItems, setFoodItems] = useState(null)
 
 
     useEffect(() => {
-        const getLocation = () => navigator.geolocation.getCurrentPosition(function(location) {
-            setLat(location.coords.latitude);
-            setLong(location.coords.longitude);
-          })
-          getLocation()
-    }, [])
+      const convertZip = async () => {
+        try {
+          const fetchZipLatAndLong = await fetch(`${process.env.REACT_APP_GOOGLE_GEOCODE_API_URL}${zipcode}`,
+            {
+              method: "GET",
+            }
+          );
+          const latAndLongData = await fetchZipLatAndLong.json();
+          if (latAndLongData.results[0]) {
+            setLat(latAndLongData.results[0].geometry.location.lat)
+            setLng(latAndLongData.results[0].geometry.location.lng)
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      };
+        convertZip()
+  }, [zipcode])
+
 
     const getFood = async () => {
       try {
-        const fetchApiData = await fetch(`${process.env.REACT_APP_FOOD_ITEMS_API_URL}lat=${lat}&lon=${long}&distance=${miles}&search=${userInput}`,
+        const fetchApiData = await fetch(`${process.env.REACT_APP_FOOD_ITEMS_API_URL}lat=${lat}&lon=${lng}&distance=${miles}&search=${userInput}`,
           {
             method: "GET",
             headers: {
@@ -31,19 +45,22 @@ function HomePage() {
         const apiResponse = await fetchApiData.json();
         setFoodItems(apiResponse.data)
         console.log(apiResponse.data)
+        document.querySelector("#zipcode").value = null;
         document.querySelector("#food-item").value = null;
-        document.querySelector("#distance").value = null;
+        document.querySelector("#miles").value = null;
       } catch (e) {
         console.log(e);
       }
     };
 
 
+
   return (
     <div>
       <h1>{foodItems ? userInput.toUpperCase() : "Home"} Page</h1>
+      <input style={{ width: "70px"}} type="number" id="zipcode" placeholder="zipcode" min="5" max="5" onChange={e => setZipcode(e.target.value)}/>
       <input type="text" id="food-item" placeholder="food item" onChange={e => setUserInput(e.target.value)} />
-      <input type="number" id="distance" placeholder="distance" min="1" max="50" onChange={e => setMiles(e.target.value)}/>
+      <input style ={{ width: "40px" }} type="number" id="miles" placeholder="miles" min="1" max="50" onChange={e => setMiles(e.target.value)}/>
       <button onClick={getFood}>Submit</button>
       <ul>
         <FoodItemList foodItems={foodItems} />
